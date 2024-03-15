c1ce175db68c0e9b34e44972cdc3881ef82b03eb
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-03-14 21:03:07 -0500

    Primer commit.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'sign-in', component: SignInComponent},
+    {path: 'sign-up', component: SignUpComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+	{path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
