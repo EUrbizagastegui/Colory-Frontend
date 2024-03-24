ce23c660adc502b3a9c6119f14c48a4d3ec33b0b
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-03-23 23:08:51 -0500

    Empezada la elaboración de los generadores de paletas de colores.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import chroma from "chroma-js";
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,11 @@ import { FooterComponent } from '../../shared/components/footer/footer.component
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isDarkTransparentBgOpen: boolean = false;
+  menuOpen: boolean = false;
+  initialColorPalette: string[] = [];
+
   navbarOptions = [
     "Extraer colores",
     "Monocromáticos",
@@ -17,11 +22,39 @@ export class HomeComponent {
     "Registrarse"
   ]
 
-  isDarkTransparentBgOpen: boolean = false;
-  menuOpen: boolean = false;
+  ngOnInit(): void {
+    this.generateColorPalette(this.initialColorPalette, 5);
+  }
 
   toggleMenu() {
     this.isDarkTransparentBgOpen = !this.isDarkTransparentBgOpen;
     this.menuOpen = !this.menuOpen;
+  }
+
+  generateColorPalette(colorsArray: string[], numberOfColors: number) {
+    let temporaryArray: chroma.Color[] = [];
+
+    for (let i = 0; i < numberOfColors; i++) {
+      const color = chroma.random();
+
+      if (temporaryArray.includes(color)) {
+        i--;
+        continue;
+      }
+
+      temporaryArray.push(color);
+    }
+
+    temporaryArray.sort((a, b) => {
+      const aHslH = a.get('hsl.h');
+      const bHslH = b.get('hsl.h');
+
+      return aHslH - bHslH;
+    });
+
+    temporaryArray.forEach(color => {
+      console.log(color.get('hsl.h'));
+      colorsArray.push(color.hex());
+    })
   }
 }
