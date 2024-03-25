3bfdf6c4772f8a716e07a11d39c81d35af256d7a
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-03-25 14:07:34 -0500

    Mejorados los algoritmos de creación de paletas de colores.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,7 +16,6 @@ export class HomeComponent implements OnInit {
   initialColorPalette: string[] = [];
   multiColorPalettes: string[][] = [];
   monoChromaticPalettes: string[][] = [];
-
   navbarOptions = [
     "Extraer colores",
     "Monocromáticos",
@@ -25,7 +24,7 @@ export class HomeComponent implements OnInit {
   ]
 
   ngOnInit(): void {
-    this.initialColorPalette = this.generateColorPalette(5);
+    this.initialColorPalette = this.generateColorGradientPalette(5);
     this.generateMultiColorPalettes(this.multiColorPalettes, 6);
   }
 
@@ -35,7 +34,7 @@ export class HomeComponent implements OnInit {
   }
 
   //Returns an array containing X random ordered colors
-  generateColorPalette(numberOfColors: number) {
+  generateRandomColorPalette(numberOfColors: number) {
     let temporaryArray: chroma.Color[] = [];
     let colorsArray: string[] = [];
 
@@ -64,10 +63,51 @@ export class HomeComponent implements OnInit {
     return colorsArray;
   }
 
-  generateMultiColorPalettes(originalArray: string[][], numberOfColors: number) {
+  //Returns an array conatining a gradient array of 5 colors between two random colors
+  generateColorGradientPalette(numberOfColors: number): string[] {
+    const randomNumber = Math.floor(Math.random() * 2);
+    const color1 = chroma.random().hex();
+    const color2 = chroma.random().hex();
+    let colorsArray: string[] = [];
+
+    switch (randomNumber) {
+      case 0:
+        colorsArray = chroma.scale([color1, color2]).colors(numberOfColors);
+        break;
+      case 1:
+        let color3 = chroma.random().hex();
+
+        do {
+          color3 = chroma.random().hex();
+        } while (color3 === color1 || color3 === color2);
+
+        colorsArray = chroma.scale([color1, color2, color3]).colors(numberOfColors);
+        break;
+      default:
+        break;
+    }
+
+    return colorsArray;
+  }
+
+  generateMultiColorPalettes(originalArray: string[][], numberOfColors: number): void {
     let temporaryArray: string[] = [];
+    let randomNumber = 0;
+
     for (let i = 0; i < numberOfColors; i++) {
-      temporaryArray = this.generateColorPalette(5);
+      //generate a random number that can be 0 or 1
+      randomNumber = Math.floor(Math.random() * 2);
+      
+      switch (randomNumber) {
+        case 0:
+          temporaryArray = this.generateRandomColorPalette(5);
+          break;
+        case 1:
+          temporaryArray = this.generateColorGradientPalette(5);
+          break;
+        default:
+          break;
+      }
 
       if (originalArray.includes(temporaryArray)) {
         i--;
